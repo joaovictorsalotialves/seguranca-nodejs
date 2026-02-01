1ab3e73a7f6aba579c2eb65a71bcb0a9ec0cfd7a
João Victor <joaovictorsalotialves12@gmail.com>
2026-02-01 14:11:40 -0300

    feat: :bug: Testando roles: adicionado os await

--- api/routes/role.js
+++ api/routes/role.js
@@ -1,7 +1,8 @@
 const { Router } = require('express')
+const RoleController = require('../controllers/roleController')
 
 const router = Router()
 
-router.post('/role').get('/role').get('/role/:id').delete('/role/:id').put('/role/:id')
+router.post('/role', RoleController.cadastrar).get('/role').get('/role/:id').delete('/role/:id').put('/role/:id')
 
 module.exports = router
