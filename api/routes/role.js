2a42cd477b827fbc1519989cc5ac4f69cc63018b
João Victor <joaovictorsalotialves12@gmail.com>
2026-02-01 14:04:12 -0300

    feat: :sparkles: Cadastrando roles

---
+++ api/routes/role.js
@@ -0,0 +1,7 @@
+const { Router } = require('express')
+
+const router = Router()
+
+router.post('/role').get('/role').get('/role/:id').delete('/role/:id').put('/role/:id')
+
+module.exports = router
