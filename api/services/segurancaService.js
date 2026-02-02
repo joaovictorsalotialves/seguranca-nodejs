f1a1aa4f01cb5e7bbee8959a08258cf483937cf3
João Victor <joaovictorsalotialves12@gmail.com>
2026-02-02 13:34:53 -0300

    feat: :sparkles: Cadastrar permissões para os perfis

--- api/services/segurancaService.js
+++ api/services/segurancaService.js
@@ -64,6 +64,49 @@ class SegurancaService {
 
     return novoUsuario
   }
+
+  async cadastrarPermissoesRoles(dto) {
+    const role = await database.roles.findOne({
+      include: [
+        {
+          model: database.permissoes,
+          as: 'role_permissao',
+          attributes: ['id', 'nome', 'descricao'],
+        },
+      ],
+    })
+
+    if (!role) {
+      throw new Error('Role não cadastrada')
+    }
+
+    const permissoesCadastradas = await database.permissoes.findAll({
+      where: {
+        id: {
+          [Sequelize.Op.in]: dto.permissoes,
+        },
+      },
+    })
+
+    await role.removeRole_permissao(role.role_permissao)
+
+    await role.addRole_permissao(permissoesCadastradas)
+
+    const novaRole = await database.roles.findOne({
+      include: [
+        {
+          model: database.permissoes,
+          as: 'role_permissao',
+          attributes: ['id', 'nome', 'descricao'],
+        },
+      ],
+      where: {
+        id: dto.roleId,
+      },
+    })
+
+    return novaRole
+  }
 }
 
 module.exports = SegurancaService
