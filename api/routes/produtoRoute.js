45c3f1063b48cc2a498c13e64e884c1323a0f706
João Victor <joaovictorsalotialves12@gmail.com>
2026-02-02 14:54:44 -0300

    feat: :sparkles: Implementando middleware permissões

--- api/routes/produtoRoute.js
+++ api/routes/produtoRoute.js
@@ -1,12 +1,13 @@
 const { Router } = require('express')
 const ProdutoController = require('../controllers/produtoController')
-const roles = require('../middlewares/role')
+// const roles = require('../middlewares/role')
+const permissoes = require('../middlewares/permissao')
 
 const router = Router()
 
 router
   .post('/produto', ProdutoController.cadastrarProduto)
-  .get('/produto', roles(['Gerente', 'Vendedor']), ProdutoController.buscarTodosProdutos)
+  .get('/produto', permissoes(['Listar']), ProdutoController.buscarTodosProdutos)
   .get('/produto/id/:id', ProdutoController.buscarProdutoPorId)
   .delete('/produto/id/:id', ProdutoController.deletarProdutoPorId)
   .put('/produto/id/:id', ProdutoController.editarProduto)
